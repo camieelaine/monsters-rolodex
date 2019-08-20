411b0f0ee7acf164c1b25827ee179a1eeb5d8c78
Camie DeCovich <camiedecovich@gmail.com>
2019-08-20 09:09:20 -0800

    Updates

---
+++ precache-manifest.e05600e0616dae8f0fdfd7dcb23f369f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc4ed0a0b9e44c0c9b3c6aa3a11287f",
+    "url": "/camieelaine/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c8ee666cccfc756df713",
+    "url": "/camieelaine/monsters-rolodex/static/css/main.c8485885.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/camieelaine/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "c8ee666cccfc756df713",
+    "url": "/camieelaine/monsters-rolodex/static/js/main.9d7d8cd6.chunk.js"
+  },
+  {
+    "revision": "b72edea15b61b1a09c6d",
+    "url": "/camieelaine/monsters-rolodex/static/js/runtime~main.bbda8685.js"
+  }
+]);
